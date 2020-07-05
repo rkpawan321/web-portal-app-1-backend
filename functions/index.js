ab327c86a341de9c9771cdfb269017649c5cd034
Pawan RK <pawan@clinikk.com>
2020-07-05 23:25:38 +0530

    sort and filter feature in get api

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
@@ -5,86 +6,92 @@ admin.initializeApp();
 
 const express = require('express');
 const app = express();
+const cors = require('cors');
+app.use(cors({ origin: true }));
 
-const projects = [
-    {
-        on_track: [
-            {
-                subject: "CBSE- Grade 5 Maths Algebra", instructors: [
-                    { id: 1, name: "Ramesh Jain" }, { id: 2, name: "Suresh Jain" }, { id: 3, name: "Bhadresh Jain" }
-                ], assigned_date: "01-03-2020", progress: 75
-            },
+let projects =
+{
+    on_track: [
+        {
+            subject: "CBSE- Grade 5 Maths Algebra", topic: "core", instructors: [
+                { id: 1, name: "Ramesh Jain" }, { id: 2, name: "Suresh Jain" }, { id: 3, name: "Bhadresh Jain" }
+            ], assigned_date: "01-03-2020", progress: 75
+        },
 
-            {
-                subject: "CBSE- Grade 5 Maths Trignometry", instructors: [
-                    { id: 1, name: "Ramesh Jain" }, { id: 3, name: "Bhadresh Jain" }
-                ], assigned_date: "20-03-2020", progress: 45
-            },
+        {
+            subject: "CBSE- Grade 5 Maths Trignometry", topic: "core", instructors: [
+                { id: 1, name: "Ramesh Jain" }, { id: 3, name: "Bhadresh Jain" }
+            ], assigned_date: "20-03-2020", progress: 45
+        },
 
-            {
-                subject: "CBSE- Grade 3 Maths Algebra", instructors: [
-                    { id: 1, name: "Ramesh Jain" }
-                ], assigned_date: "01-01-2020", progress: 20
-            }
-        ]
-    },
-    {
-        delayed: [
-            {
-                subject: "CBSE- Grade 2 Maths English", instructors: [
-                    { id: 6, name: "Ramesh Shetty" }, { id: 7, name: "Rama Joe" }, { id: 8, name: "Jesus Christ" }
-                ], assigned_date: "01-03-2020", progress: 75
-            },
+        {
+            subject: "CBSE- Grade 3 English", topic: "language", instructors: [
+                { id: 1, name: "Ramesh Jain" }
+            ], assigned_date: "01-01-2020", progress: 20
+        }
+    ],
 
-            {
-                subject: "CBSE- Grade 1 Maths Science", instructors: [
-                    { id: 4, name: "John Doe" }, { id: 5, name: "John Cena" }
-                ], assigned_date: "20-03-2020", progress: 45
-            },
-        ]
-    },
-    {
-        on_hold: [
-            {
-                subject: "CBSE- Grade 2 Social Science ", instructors: [
-                    { id: 6, name: "Ramesh Shetty" }, { id: 7, name: "Rama Joe" }, { id: 8, name: "Jesus Christ" }
-                ], assigned_date: "09-05-2020", progress: 55
-            },
+    delayed: [
+        {
+            subject: "CBSE- Grade 7 French", topic: "language", instructors: [
+                { id: 6, name: "Ramesh Shetty" }, { id: 7, name: "Rama Joe" }, { id: 8, name: "Jesus Christ" }
+            ], assigned_date: "01-03-2020", progress: 75
+        },
 
-            {
-                subject: "CBSE- Grade 5 Moral Science", instructors: [
-                    { id: 4, name: "John Doe" }, { id: 5, name: "John Cena" }
-                ], assigned_date: "01-01-2020", progress: 90
-            },
+        {
+            subject: "CBSE- Grade 1 Maths Science", topic: "core", instructors: [
+                { id: 4, name: "John Doe" }, { id: 5, name: "John Cena" }
+            ], assigned_date: "20-03-2020", progress: 45
+        },
+    ],
 
-        ]
+    on_hold: [
+        {
+            subject: "CBSE- Grade 2 Social Science ", topic: "core", instructors: [
+                { id: 6, name: "Ramesh Shetty" }, { id: 7, name: "Rama Joe" }, { id: 8, name: "Jesus Christ" }
+            ], assigned_date: "09-05-2020", progress: 55
+        },
+
+        {
+            subject: "CBSE- Grade 5 Hindi", topic: "language", instructors: [
+                { id: 4, name: "John Doe" }, { id: 5, name: "John Cena" }
+            ], assigned_date: "01-01-2020", progress: 90
+        },
+
+    ]
+}
+
+const getSortedSubjects = (projects, sort) => {
+    let filteredProjects = {
+        on_track: _.filter(projects.on_track, ['topic', sort]),
+        delayed: _.filter(projects.delayed, ['topic', sort]),
+        on_hold: _.filter(projects.on_hold, ['topic', sort]),
     }
-];
+    return filteredProjects
+}
 
-exports.getProjects = functions.https.onRequest((request, response) => {
-    response.set("Access-Control-Allow-Origin", "*"); // you can also whitelist a specific domain like "http://127.0.0.1:4000"
-    response.set("Access-Control-Allow-Headers", "Content-Type");
-    response.send(projects);
+
+app.get('/projects', (req, res) => {
+    const { field, sort, filter } = req.query;
+    console.info('request.query', req.query)
+    let projectResponse = projects;
+    if (filter === 'on_track') {
+        projectResponse = { on_track: projects.on_track };
+    } else if (filter === 'on_hold') {
+        projectResponse = { on_hold: projects.on_hold };
+    } else if (filter === 'delayed') {
+        projectResponse = { delayed: projects.delayed };
+    } else {
+        projectResponse === projects;
+    }
+    if (sort === 'language') {
+        projectResponse = getSortedSubjects(projectResponse, 'language')
+    } else if (sort === 'core') {
+        projectResponse = getSortedSubjects(projectResponse, 'core')
+    } else {
+        projectResponse = projectResponse;
+    }
+    return res.send(projectResponse)
 });
 
-// app.get('/projects', (req, res) => {
-//     admin.
-//         firestore()
-//         .collection('projects')
-//         .get()
-//         .then(data => {
-//             let projects = [];
-//             data.forEach(doc => {
-//                 projects.push({
-//                     projectId: doc.id,
-//                     on_track: doc.data().on_track,
-//                     delayed: doc.data().delayed,
-//                     on_hold: doc.data().on_hold,
-//                 });
-//             });
-//             return res.json(projects);
-//         })
-//         .catch(err => console.error(err));
-// });
-
-// exports.api = functions.https.onRequest(app);
+exports.api = functions.https.onRequest(app);
